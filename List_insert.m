c605d12f59cf9fc207eddacb1ef5bea6279f19d9
KsuZZ <35704204+KsuZZ@users.noreply.github.com>
2018-02-01 20:39:04 +0300

    Delete List_insert.m

--- List_insert.m
+++
@@ -1,8 +0,0 @@
-function S=List_insert(S, x, key)  %вставка списки
-Z(1:x-1)=[S(1:x-1)];
-U(x:length(S))=[S(x:length(S))];
-U=U(x:length(S));
-
-S(x).key=key;
-Z(x-1).next=S(x).key;
-S(x).next=U(1).key;
